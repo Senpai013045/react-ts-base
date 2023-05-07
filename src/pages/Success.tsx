a52c51ac6b3f4898abfe884e1ec16a06e0b8b76a
Subham Shrestha <subham.hero01@gmail.com>
2023-05-07 21:34:58 +0545

    final 7th

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -1,33 +1,53 @@
-import {Navigate} from "react-router-dom";
+import {Link, Navigate} from "react-router-dom";
 import {Container} from "../components/Layout/Container";
 import {GAMES, BUYING_OPTIONS} from "../data/games";
+import {Button} from "../components/UI/Button";
+import {useMemo} from "react";
 
 export const Success = () => {
   const searchParams = new URLSearchParams(window.location.search);
   const gameSlug = searchParams.get("game");
   const index = Number(searchParams.get("index"));
+  const gamerId = searchParams.get("gamerId");
   const game = GAMES.find(game => game.slug === gameSlug);
   const buyingOption = BUYING_OPTIONS[index];
+  const transactionId = useMemo(() => {
+    const random = Math.random().toString(36);
+    //ignore the first 2 characters, dont use substring
+    return random.slice(2);
+  }, []);
 
   if (!game || index < 0) {
     return <Navigate to="/" />;
   }
 
   return (
-    <Container className="py-8">
-      <h3 className="text-2xl font-bold">Success</h3>
-      <main className="mt-8">
-        <h4 className="text-xl font-bold">Thank you for your purchase</h4>
-        <p className="mt-4">
-          You have successfully purchased {buyingOption.base} {game.currency.name}{" "}
-          {buyingOption.bonus > 0 && `with bonus of ${buyingOption.bonus} ${game.currency.name}`}{" "}
-          for {game.name}
-        </p>
-        <p className="mt-4">Transaction ID: {Math.floor(Math.random() * 10000000000000000)}</p>
-        {/* date and time */}
-        <p className="mt-4">Date: {new Date().toLocaleDateString()}</p>
-        <p className="mt-4">Time: {new Date().toLocaleTimeString()}</p>
-      </main>
+    <Container className="py-8 flex items-center justify-center h-screen">
+      <div className="translate-y-[-50%]">
+        <h3 className="text-2xl font-bold">Success</h3>
+        <main className="mt-8">
+          <h4 className="text-xl font-bold">Thank you for your purchase</h4>
+          <p className="mt-4">
+            You have successfully purchased {buyingOption.base} {game.currency.name}{" "}
+            {buyingOption.bonus > 0 && `with bonus of ${buyingOption.bonus} ${game.currency.name}`}{" "}
+            for {game.name}
+          </p>
+          <p className="mt-4">Your account has been credited with {buyingOption.price} USD.</p>
+          <p className="mt-4">Gamer ID: {gamerId}</p>
+          <p className="mt-4">Transaction ID: {transactionId}</p>
+          {/* date and time */}
+          <p className="mt-4">Date: {new Date().toLocaleDateString()}</p>
+          <p className="mt-4">Time: {new Date().toLocaleTimeString()}</p>
+          <div className="mt-8 flex gap-x-4 items-center">
+            <Link to={`/`}>
+              <Button>Back to Shop</Button>
+            </Link>
+            <Link to={`/shop/${gameSlug}`}>
+              <Button>Buy Again</Button>
+            </Link>
+          </div>
+        </main>
+      </div>
     </Container>
   );
 };
--- src/pages/Verification.tsx
+++ src/pages/Verification.tsx
@@ -10,6 +10,9 @@ export const Verification = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [verificationCode, setVerificationCode] = useState("");
+  const [gamerIdVerificationError, setGamerIdVerificationError] = useState("");
+  const [gamerVerified, setGamerVerified] = useState(false);
+  const [gamerId, setGamerId] = useState("");
   const params = useParams();
   const navigate = useNavigate();
   const index = Number(params.index);
@@ -19,6 +22,44 @@ export const Verification = () => {
   if (!game || index < 0) {
     return <Navigate to="/" />;
   }
+
+  const handleVerification = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      if (!gamerId) {
+        setGamerIdVerificationError("Please enter a valid gamer Id");
+        setGamerVerified(false);
+        setIsLoading(false);
+
+        return;
+      }
+      //only numbers are allowed
+      if (!/^\d+$/.test(gamerId)) {
+        setGamerIdVerificationError("Gamer id invalid");
+        setGamerVerified(false);
+        setIsLoading(false);
+
+        return;
+      }
+      //gamer id should be 10 characters long
+      if (gamerId.length !== 10) {
+        setGamerIdVerificationError("Gamer id invalid");
+        setGamerVerified(false);
+        setIsLoading(false);
+        return;
+      }
+      //if ends in 90 it is invalid
+      if (gamerId.endsWith("90")) {
+        setGamerIdVerificationError("Gamer id invalid");
+        setGamerVerified(false);
+        setIsLoading(false);
+        return;
+      }
+      setGamerIdVerificationError("");
+      setGamerVerified(true);
+      setIsLoading(false);
+    }, 2000);
+  };
 
   return (
     <Container className="py-8">
@@ -46,7 +87,7 @@ export const Verification = () => {
               onClick={() => {
                 setIsLoading(true);
                 setTimeout(() => {
-                  navigate(`/success?game=${game.slug}&index=${index}`);
+                  navigate(`/success?game=${game.slug}&index=${index}&gamerId=${gamerId}`);
                 }, 1000);
               }}
             >
@@ -65,35 +106,63 @@ export const Verification = () => {
           </div>
         </div>
         <div className="flex-1">
-          <div className="bg-ui-darker mt-4">
-            {/* display selected */}
-            <div className="flex justify-between items-center gap-2 p-2">
-              <div className="flex items-center gap-2">
-                <img src={game.currency.image} alt={game.name} className="w-8 h-8" />
-                {BUYING_OPTIONS[index].base} {game.currency.name}
-              </div>
-              <div className="flex items-center gap-2">
-                Bonus
-                <img src={game.currency.image} alt={game.name} className="w-8 h-8" />
-                {BUYING_OPTIONS[index].bonus} {game.currency.name}
+          {gamerVerified && (
+            <div className="bg-ui-darker mt-4">
+              {/* display selected */}
+              <div className="flex justify-between items-center gap-2 p-2">
+                <div className="flex items-center gap-2">
+                  <img src={game.currency.image} alt={game.name} className="w-8 h-8" />
+                  {BUYING_OPTIONS[index].base} {game.currency.name}
+                </div>
+                <div className="flex items-center gap-2">
+                  Bonus
+                  <img src={game.currency.image} alt={game.name} className="w-8 h-8" />
+                  {BUYING_OPTIONS[index].bonus} {game.currency.name}
+                </div>
               </div>
             </div>
+          )}
+
+          {/* gamer id verification */}
+          <div className="mt-8 bg-ui-da">
+            <h4 className="text-xl font-bold">Gamer ID Verification</h4>
+            <p className="mt-4">Please enter your gamer ID to receive your in-game currency</p>
+            <div className="mt-4">
+              <label className="text-sm font-bold">Gamer ID</label>
+              <Input
+                value={gamerId}
+                onChange={e => {
+                  setGamerId(e.target.value);
+                  setGamerVerified(false);
+                }}
+                type="text"
+                className="w-full"
+              />
+            </div>
+            <p className="mt-2 text-red-500">{gamerIdVerificationError}</p>
+            {!gamerVerified && (
+              <Button className="w-full mt-4" onClick={handleVerification}>
+                {isLoading ? "Processing" : "Verify"}
+              </Button>
+            )}
           </div>
           {/* mock payment */}
-          <div className="mt-8">
-            <Button
-              className="w-full"
-              onClick={() => {
-                setIsLoading(true);
-                setTimeout(() => {
-                  setIsLoading(false);
-                  setIsModalOpen(true);
-                }, 3000);
-              }}
-            >
-              {isLoading ? "Processing" : "Pay with Connected Wallet"}
-            </Button>
-          </div>
+          {gamerVerified && (
+            <div className="mt-8">
+              <Button
+                className="w-full"
+                onClick={() => {
+                  setIsLoading(true);
+                  setTimeout(() => {
+                    setIsLoading(false);
+                    setIsModalOpen(true);
+                  }, 3000);
+                }}
+              >
+                {isLoading ? "Processing" : "Pay with Connected Wallet"}
+              </Button>
+            </div>
+          )}
 
           {/* mock payment end */}
         </div>
